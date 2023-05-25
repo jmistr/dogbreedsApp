cd4a797ad261f5f3614ad5a402b6b96260ec7a31
jmistr <121116548+jmistr@users.noreply.github.com>
2023-05-25 14:37:05 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -14,8 +12,6 @@ import { AppComponent } from './app.component';
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
-    MatFormFieldModule,
-    MatSelectModule,
     BrowserAnimationsModule,
   ],
   declarations: [AppComponent],
